7b5f2116eaf3231d276979a3fb4109fda9a726b9
leithdm@gmail.com <darrenm.leith@lethalapps.com>
2016-03-21 12:30:36 +0000

    feat: Basic image download

--- Astronomy Picture Of The Day - NASAs APOD/ViewController.swift
+++ Astronomy Picture Of The Day - NASAs APOD/ViewController.swift
@@ -9,17 +9,59 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+	
+	@IBOutlet weak var imageAPOD: UIImageView!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		
+		parseAPI { (data) -> Void in
+			let url = NSURL(string: data)
+			let imageData = NSData(contentsOfURL: url!)
+			dispatch_async(dispatch_get_main_queue()) {
+				self.imageAPOD.image = UIImage(data: imageData!)
+			}
+		}
 	}
 
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+	
+	func dateToString(date: NSDate) -> String {
+		let dateFormatter = NSDateFormatter()
+		dateFormatter.dateFormat = "YYYY-MM-DD"
+		return dateFormatter.stringFromDate(date)
 	}
-
-
+	
+	
+	func parseAPI(completionHandler: (data: String) -> Void) {
+		
+		let stringURL = APODAPI.Constants.BASE_URL + APODAPI.Constants.API_KEY
+		let session = NSURLSession.sharedSession()
+		let request = NSURLRequest(URL: NSURL(string: stringURL)!)
+		let task = session.dataTaskWithRequest(request) { (data, response, error) -> Void in
+			//guard statements for error, response
+			
+			guard let data = data else {
+				//error
+				return
+			}
+			
+			//parse the data
+			var parsedData: AnyObject!
+			
+			do {
+				parsedData = try NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments) as! [String: AnyObject]
+			} catch {
+				parsedData = nil
+				return
+			}
+			
+			guard let url = parsedData["url"] as? String else { //for example
+				print("error")
+				return
+			}
+			print(url)
+			completionHandler(data: url)
+		}
+		task.resume()
+	}
 }
-
