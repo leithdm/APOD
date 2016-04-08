21add65e4afd92fae5a0d1e860ccfd5acf61ceb6
leithdm@gmail.com <darrenm.leith@lethalapps.com>
2016-04-08 16:26:51 +0100

    docs: code cleanup

--- Astronomy Picture Of The Day - NASAs APOD/AppDelegate.swift
+++ Astronomy Picture Of The Day - NASAs APOD/AppDelegate.swift
@@ -19,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	}
 	
 	//MARK: properties
+	
 	var childViewControllers = [UIViewController]()
 	var childNav: UINavigationController!
 	var menuNav: UINavigationController!
